e34e0d8e1f052cfd3498407155c8ce55354dae16
beverleyngo <beverleyngo168@yahoo.co.uk>
2021-12-02 21:54:02 +0000

    Added comments

--- day2_puzzle1.py
+++ day2_puzzle1.py
@@ -1,17 +1,16 @@
+# Read the input file into two lists containing the commands and the units
 f=open('day2_input.txt',"r")
 commands = []
 units = []
-
 for line in f:
     commands.append(line.split(' ')[0])
     units.append(int(line.split(' ')[1]))
 
-# print(commands)
-# print(units)
-
+# Set the initial horizontal and depth to 0
 horizontal = 0
 depth = 0
 
+# Iterate through the commands, updating the horizontal and depth accordingly
 for i in range(len(commands)):
     if commands[i] == 'forward':
         horizontal += units[i]
